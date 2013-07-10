7944b3ff08e5e0233cca77b8026d950eff45972c
Narciso Albarracin <nalbarraci@hotmail.com>
2013-07-10 04:15:26 +0000

    Updated web.js to read index.html and return a response

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+fs.readFileSync('index.html', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+});
+var buffer = new Buffer(data);
+var out = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
